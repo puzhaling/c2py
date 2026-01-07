a643d9f15cf95334ee7e8000d5d9ede760589bbb
mqwl <141118776+mqwl@users.noreply.github.com>
2026-01-07 15:02:28 +1000

    returned expression translator header

---
+++ include/expr_translator.h
@@ -0,0 +1,15 @@
+// Header файл преобразователя выражений
+
+#pragma once
+#include "token.h"
+#include <vector>
+#include <string>
+
+class ExpressionTranslator {
+public:
+    // Преобразование выражений
+    std::string translate(const std::vector<Token>& tokens);
+private:
+    // Преобразование операторов
+    std::string mapOperator(const std::string& op);
+};
